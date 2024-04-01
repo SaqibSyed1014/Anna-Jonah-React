fcee65eb5b36d7240282b2c835f9a36720ab9cbe
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-04-01 13:15:09 +0500

    images updated

--- src/pages/Home/components/Hero.jsx
+++ src/pages/Home/components/Hero.jsx
@@ -31,8 +31,8 @@ function Hero() {
     return (
         <div className="header-section">
             <div className="header">
-                <div className="header-jol"><img src="/public/images/jol.svg" loading="lazy" alt="" className="image-2" /></div>
-                <div id="animatedElement" className="header-dishes"><img src="/public/images/dishes-pyramid.webp" loading="eager" sizes="(max-width: 991px) 100vw, (max-width: 1279px) 1031.995361328125px, 100vw" srcSet="/images/dishes-pyramid-p-500.webp 500w, /images/dishes-pyramid-p-800.webp 800w, /images/dishes-pyramid.webp 1024w" alt="" className="image-3" /></div>
+                <div className="header-jol"><img src="/images/jol.svg" loading="lazy" alt="" className="image-2" /></div>
+                <div id="animatedElement" className="header-dishes"><img src="/images/dishes-pyramid.webp" loading="eager" sizes="(max-width: 991px) 100vw, (max-width: 1279px) 1031.995361328125px, 100vw" srcSet="/images/dishes-pyramid-p-500.webp 500w, /images/dishes-pyramid-p-800.webp 800w, /images/dishes-pyramid.webp 1024w" alt="" className="image-3" /></div>
             </div>
         </div>
     )
